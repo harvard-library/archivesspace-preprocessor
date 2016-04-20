3126c12f766e1e79385acacdd109824f7ddc19a8
Dave Mayo <dave_mayo@harvard.edu>
2016-04-20 17:07:07 -0400

    Zip input and output!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def pp_time(time)
     time.strftime Rails.configuration.x.time_display_fmt
   end
+
+  def output_path(run, eadid = nil)
+    "output/#{run.id.to_s}/#{eadid ? "#{eadid}.xml" : 'out.zip'}"
+  end
+
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,5 @@
+require 'java'
+
 # A process consisting of the following steps, repeated over
 # the current version of each FindingAid:
 #
@@ -8,7 +10,8 @@
 # 5. Save final XML result to file
 class Run < ActiveRecord::Base
   # Directory to output processed files
-  OUTPUT_DIR = File.join(Rails.root, 'system', 'output')
+  OUTPUT_DIR = File.join(Rails.root, 'public', 'output')
+  INPUT_DIR =  File.join(Rails.root, 'public', 'input')
 
   belongs_to :schematron
   has_and_belongs_to_many :finding_aid_versions
@@ -36,8 +39,13 @@ class Run < ActiveRecord::Base
   def perform_processing!
     raise "This run is already processed!" if run_for_processing
     update(run_for_processing: true)
-    outdir = File.join(OUTPUT_DIR, "#{id}")
+    outdir = File.join(OUTPUT_DIR, "#{id}").shellescape
+    indir =  File.join(INPUT_DIR,  "#{id}").shellescape
     Dir.mkdir(outdir, 0700) unless File.directory?(outdir)
+    Dir.mkdir(indir, 0700) unless File.directory?(indir)
+
+    # Stream input files to zip
+    zout = java.util.zip.ZipOutputStream.new(File.open(File.join(indir, 'input.zip'), 'wb').to_outputstream)
 
     finding_aid_versions
       .joins(:finding_aid, :concrete_issues => :issue)
@@ -46,32 +54,57 @@ class Run < ActiveRecord::Base
                ARRAY_AGG(DISTINCT issues.identifier) AS identifiers')
       .group('finding_aids.eadid,finding_aid_versions.id')
       .each do |fa|
-        # Apply all relevant fixes to Finding Aid
-      repaired = Fixes
-                 .to_h
-                 .select {|issue_id, _| fa.identifiers.include? issue_id}
-                 .reduce(fa.xml) do|xml, (issue_id, fix)|
-        pe = processing_events.create(issue_id: Issue.find_by(identifier: issue_id).id,
-                                      finding_aid_version_id: fa.id)
-        pre_fix_xml = xml.dup
-        begin # In case of failure, catch the XML
-          fix.(xml)
-        rescue Fixes::Failure => e
-          pe.update(failed: true)
-          pre_fix_xml
-        end
-      end # end of .reduce
+        add_to_input_zip(zout, fa)
 
-      File.open(File.join(outdir, "#{fa.eadid}.xml"), 'w') do |f|
-        repaired.write_xml_to(f, encoding: 'UTF-8')
-      end
+        # Apply all relevant fixes to Finding Aid
+        repaired = Fixes
+                   .to_h
+                   .select {|issue_id, _| fa.identifiers.include? issue_id}
+                   .reduce(fa.xml) do|xml, (issue_id, fix)|
+          pe = processing_events.create(issue_id: Issue.find_by(identifier: issue_id).id,
+                                        finding_aid_version_id: fa.id)
+          pre_fix_xml = xml.dup
+          begin # In case of failure, catch the XML
+            fix.(xml)
+          rescue Fixes::Failure => e
+            pe.update(failed: true)
+            pre_fix_xml
+          end
+
+        end # end of .reduce
+
+        File.open(File.join(outdir, "#{fa.eadid}.xml"), 'w') do |f|
+          repaired.write_xml_to(f, encoding: 'UTF-8')
+        end
     end
+
+    system("find #{outdir} -name '*.xml' | zip #{File.join(outdir, 'out')} -j -@")
     update(completed_at: DateTime.now)
+  ensure
+    begin
+      zout.close # Input zip done
+    rescue java.io.IOException => e
+      # already closed, nothing to do here
+    end
   end
 
   # Convenience method for doing analysis and processing in one go.
   def perform_processing_run(faids)
     perform_analysis(faids)
     perform_processing!
+  end
+
+  # Convenience method for adding to input zip
+  # @param zout [Java::Util::Zip::ZipOutputStream] directory to store zip in
+  # @param fav [FindingAidVersion] version to add to zip
+  def add_to_input_zip(zout, fav)
+    zout.put_next_entry(java.util.zip.ZipEntry.new("#{fav.eadid}.xml"))
+    file = fav.file
+    file.binmode
+    file.each_line do |line|
+      bytes = line.to_java_bytes
+      zout.write(bytes, 0, bytes.length)
+    end
+    file.close
   end
 end
