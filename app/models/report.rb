c71abacb0aae608dfd48dfa89f3ff7e28ebb903e
Dave Mayo <pobocks@gmail.com>
2022-09-14 00:36:01 -0400

    Report on manual issues

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,4 @@
+require 'set'
 # Various reports, aggregated here for convenience
 class Report
   # Returns a report of issues found per repository
@@ -20,4 +21,21 @@ class Report
         [k, v.map {|el| [el[0], el[2]]}.to_h] # Drop message from vals, then hashify
       end.to_h
   end
+
+  def self.manual_issues(run)
+    manual_issues = Set.new(run.schematron.issues.where("identifier NOT IN (?)", Fixes.to_h.keys).pluck('identifier'))
+    Run.
+      joins(finding_aid_versions: [
+              {concrete_issues: :issue},
+              :finding_aid
+            ]).
+      where({'runs.id' => run.id, 'issues.identifier' => manual_issues}).
+      group('eadid', 'issues.id', Arel.sql("issues.identifier || ' - ' || issues.message")).
+      order('eadid', 'issues.id').
+      pluck('eadid', Arel.sql("issues.identifier || ' - ' || issues.message"), Arel.sql('array_agg(DISTINCT concrete_issues.line_number)'))
+
+    # Todo: put this into some kind of usable data format and display it somewhere?
+
+  end
+
 end
